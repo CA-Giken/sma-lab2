1496eb5a1ee366ea309f11a85b92d94b1cdb013e
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2025-06-13 12:22:27 +0900

    Changed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-  packages=['sma-lab2'],
+  packages=['sma_lab2'],
   package_dir={'': 'src'},
   scripts=[]
 )
